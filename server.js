bce031be89db1a7563ba417693ceff88ee8b3311
metages worku <metages09@gmail.com>
2021-01-22 12:16:25 -0800

    deploying to heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 //   useFindAndModify: false
 // });
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb://localhost/Budget',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
